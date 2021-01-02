d96a57e08ba800eed0d34c1c8549914fc12e1fc4
Psi505 <hamzusguesmi@gamil.com>
2021-01-03 00:29:04 +0100

    first version

---
+++ README.md
@@ -0,0 +1,5 @@
+This is a very simple program that can be used in your batch projects as a function to diplay a very enhanced
+GUI Buttons.
+
+ * Follow the example for usage.
+ * There is comments in the "Button.bat" file to help understand how the program works. 
---
+++ Test_Button_Function.bat
@@ -0,0 +1,14 @@
+@Echo off
+@Mode 48,15
+@Title %~n0
+Batbox /h 0
+
+:Loop
+Call Button  10 4 "Open cmd" 24 4 "Close Cmd" 17 8 "  Exit  " # Press
+Getinput /m %Press% /h 70
+
+:: Check for the pressed button 
+if %errorlevel%==1 (start "Cmd")
+if %errorlevel%==2 (taskkill /f /fi "windowtitle eq Cmd" >nul 2>&1)
+if %errorlevel%==3 (exit)
+goto Loop
